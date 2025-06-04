d54283f55867f01d1ba9ea5e715dadaa18237576
Thinh Phuc <duonge12@gmail.com>
2025-06-04 12:23:05 +0700

    finish banner

--- src/js/loadComponent.js
+++ src/js/loadComponent.js
@@ -9,20 +9,6 @@ const handleScript = async (href) => {
 };
 
 const handleLoadComponent = async (href, script) => {
-	const container = document.querySelector("section.content");
-	const response = await fetch(href);
-	if (!response.ok) {
-		container.innerHTML = response.statusText;
-	} else {
-		const result = await response.text();
-		container.innerHTML = result;
-		if (script) {
-			const scriptText = await handleScript(script);
-			eval(scriptText);
-		}
-	}
-};
-const handleLoadHeader = async (href, script) => {
 	const tempContainer = document.createElement("div");
 	const response = await fetch(href);
 	if (!response.ok) {
@@ -30,8 +16,8 @@ const handleLoadHeader = async (href, script) => {
 	} else {
 		const result = await response.text();
 		tempContainer.innerHTML = result;
-		const header = tempContainer.querySelector("header");
-		document.body.insertBefore(header, document.body.firstChild);
+		const section = tempContainer.firstChild;
+		document.body.appendChild(section);
 		if (script) {
 			const scriptText = await handleScript(script);
 			eval(scriptText);
